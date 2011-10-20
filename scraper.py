32d7ff61bb115e002eeb29865d63d1d463b4e036
Wesley Zhao <wesley.zhao@gmail.com>
2011-10-19 20:17:33 -0700

    added sys.argv input availability

--- scraper.py
+++ scraper.py
@@ -2,12 +2,31 @@ import requests
 import lxml
 from lxml.html import parse
 import csv
+import sys
 GOOGLE_URL = "http://www.google.com/search?q="
-FILE_PATH = "/home/wesley/Downloads/"
-FILE_NAME = "d3schools.csv"
+IN_FILE_PATH = "/home/wesley/Downloads/d3schools.csv"
+OUT_FILE_PATH = "/home/wesley/Downloads/scraper_result.csv"
+
 def main():
-    rows = get_athletic_sites_from_file(FILE_PATH+FILE_NAME)
-    write_athletic_sites_to_file(FILE_PATH+"scraper_result.csv", rows)
+    """ 
+    Take an input csv file with College names as each row, and outputs 
+    a file with the college name, the google search result title, and
+    the google search result link as the rows
+
+    Syntax:
+    >> python scraper.py /path/to/input.csv /path/to/output.csv
+
+    Otherwise, IN_FILE_PATH and OUT_FILE_PATH are the defaults for 
+    the location/names of the input/output csv
+    """
+    try:
+        # try to get the file input/output from sys.argv
+        IN_FILE_PATH = sys.argv[1] # first arg is the input file
+        OUT_FILE_PATH = sys.argv[2] # second arg is the output file
+    except KeyError:
+        print "To enter filepaths from console, use the following syntax >> python scraper.py /path/to/input.csv /path/to/output.csv"
+    rows = get_athletic_sites_from_file(IN_FILE_PATH)
+    write_athletic_sites_to_file(OUT_FILE_PATH, rows)
 
 def get_athletic_sites_from_file(fpath):
     reader = csv.reader(open(fpath, "rb"))
@@ -26,7 +45,7 @@ def write_athletic_sites_to_file(fpath, iterable):
             writer.writerow(row)
         except UnicodeEncodeError:
             print "THERE WAS A UNICODE ERROR FOR %s" %str(row)
-            writer.writerow([row[0],"",row[2]])
+            writer.writerow([row[0],"",row[2]]) # most of the unicode errors are a result of the Google Search Result Title, so replace it with empty string
         print "row written."
     # writer.writerows(iterable)
 
@@ -48,3 +67,6 @@ def get_top_google_result(search_query):
         print "wtf no search result for %s" %search_query
         return "", ""
     return first_result.text_content(), first_result.get('href')
+
+if __name__ == "__main__":
+    main()
